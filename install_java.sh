e51edc55c5df90b9893f5884fd82b0f480f77fae
Vasily Kolpakov <vasilykolpakov@gmail.com>
2017-12-15 18:24:16 +0300

    Fix install_java.sh script

--- install_java.sh
+++ install_java.sh
@@ -2,7 +2,7 @@
 set -o xtrace
 JDK_DIR=`tar ft $1 | head -n 1 | sed 's/.$//'`
 tar xf $1
-sudo mkdir -p /usr/list/jvm
+sudo mkdir -p /usr/lib/jvm
 sudo mv $JDK_DIR /usr/lib/jvm
 
 JAVA_COMMANDS="java
@@ -10,9 +10,11 @@ javac
 jar
 jps
 jmc
+jjs
 jstat
+javah
 jstack"
 
 for COMMAND in $JAVA_COMMANDS; do
-  sudo update-alternatives --install "/usr/bin/$COMMAND" $COMMAND "/usr/lib/jvm/$JDK_DIR/bin/$COMMAND" 1
+  sudo update-alternatives --install "/usr/bin/$COMMAND" $COMMAND "/usr/lib/jvm/$JDK_DIR/bin/$COMMAND" 2000
 done
